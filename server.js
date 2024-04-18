e8144de84b07866e60228921b588486cff3288df
Srivatsan <vatsan1993@gmail.com>
2024-04-18 11:26:44 +0530

    finished program

--- server.js
+++ server.js
@@ -3,10 +3,33 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   // Your code here
+  if (req.method == 'GET' && req.url.startsWith('/static')) {
+    let urlParts = req.url.split('/');
+    if (urlParts[1] == 'static' && urlParts[2] == 'images') {
+      if (urlParts[3] == 'dog.jpg') {
+        let image = fs.readFileSync('./assets/images/dog.jpg');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'image/jpg');
+
+        return res.end(image);
+      }
+    }
+
+    if (urlParts[1] == 'static' && urlParts[2] == 'css') {
+      if (urlParts[3] == 'application.css') {
+        let cssFile = fs.readFileSync('./assets/css/application.css');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+
+        return res.end(cssFile);
+      }
+    }
+  }
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   let indexFile = fs.readFileSync('./index.html');
-  res.end(indexFile);
+  return res.end(indexFile);
 });
 
 const port = 5000;
