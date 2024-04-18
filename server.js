fa7847cd4ef40ce1896f563d702f8b36ad50ba85
Srivatsan <vatsan1993@gmail.com>
2024-04-18 10:22:21 +0530

    phase 1 finished

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
-const fs = require("fs");
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   // Your code here
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  let indexFile = fs.readFileSync('./index.html');
+  res.end(indexFile);
 });
 
 const port = 5000;
